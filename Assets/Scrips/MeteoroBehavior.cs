bfa4121cc64aee65b07e11bba1edaed2acc5641b
David Soto <urielsonic@gmail.com>
2018-02-12 10:45:13 -0600

    Se subieron los Assets, Proyect Settings y Unity Manager

---
+++ Assets/Scrips/MeteoroBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Autores Rodrigo Samayoa y David Soto
+
+public class MeteoroBehavior : MonoBehaviour {
+    public float speed = 4f;
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        rb.transform.Translate(Vector2.down * speed * Time.deltaTime); //Forma en que se desplazaran los asteroides
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "laserBlue12 (2)") //Condicion para desaparecer los asteroides
+        {
+            Destroy(gameObject); //Destruye el asteroide
+            //Debug.Log(collision.gameObject.name);
+        }
+    }
+}
