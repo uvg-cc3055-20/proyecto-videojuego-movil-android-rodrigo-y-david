ada8956b1502c0b4d5a11a0fcff0e1e07b750a06
sam17332 <32148906+sam17332@users.noreply.github.com>
2018-02-19 18:53:20 -0600

    se agregan botones para mover la nave.

--- Assets/Scrips/Ship.cs
+++ Assets/Scrips/Ship.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 //Autores Rodrigo Samayoa y David Soto
 
 public class Ship : MonoBehaviour {
-    public float speed = 4f; //Velocidad a la que se movera la nave
+    //public float speed = 4f; //Velocidad a la que se movera la nave
     Rigidbody2D rb;
 	
 	[Header("Disparo")]
@@ -20,10 +20,10 @@ public class Ship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float movX = Input.acceleration.x; //variable que instancia el acelerometro del dispositivo en x
-        float movY = Input.acceleration.y; //variable que instancia el acelerometro del dispositivo en y
-        rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime); //El comportamiento de como se trasladara el objeto en x
-        rb.transform.Translate(Vector2.up * speed * movY * Time.deltaTime); //El comportamiento de como se trasladara el objeto en y
+        //float movX = Input.acceleration.x; //variable que instancia el acelerometro del dispositivo en x
+        //float movY = Input.acceleration.y; //variable que instancia el acelerometro del dispositivo en y
+        //rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime); //El comportamiento de como se trasladara el objeto en x
+        //rb.transform.Translate(Vector2.up * speed * movY * Time.deltaTime); //El comportamiento de como se trasladara el objeto en y
 	    
 	    if (Input.GetButton ("Fire1") && Time.time > nextShot) 
 	    {
---
+++ Assets/Scrips/movimientoNave.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoNave : MonoBehaviour {
+
+// En esta variable configuraremos la velocidad a la que se moverá el objeto
+    public float Velocidad = 5.0F;
+ 
+    private bool derecha;
+    private bool izquierda;
+ 
+    void Update ()
+    {
+        if(Input.GetButton ("Fire1") && derecha)
+        {
+            // Movemos el objeto hacia la derecha
+            this.transform.Translate(Vector3.right * Time.deltaTime * Velocidad);
+        }
+ 
+        if (Input.GetButton ("Fire1") && izquierda)
+        {
+            // Movemos el objeto hacia la izquierda
+            this.transform.Translate(Vector3.left * Time.deltaTime * Velocidad);
+        }
+    }
+ 
+    /******************** FUNCIONES PÚBLICAS ********************/
+ 
+    public void MoverDerecha()
+    {
+        derecha = true;
+    }
+ 
+    public void MoverIzqda()
+    {
+        izquierda = true;
+    }
+    
+    public void MoverDere()
+    {
+        derecha = false;
+    }
+ 
+    public void MoverIzquierda()
+    {
+        izquierda = false;
+    }
+}
